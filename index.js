9028960063fc025077aab1ab53138a0153ff835b
rarasomething <yjung2027@chadwickschool.org>
2022-10-29 15:57:40 +0900

    initial push

---
+++ index.js
@@ -0,0 +1,56 @@
+import React, {useState} from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    background-color: #f5f5f5;
+    height: 100vh;
+    text-align: center;
+`;
+
+const Title = styled.div`
+    font-size: 70px;
+    color: lightslategrey;
+    padding-top: 100px;
+    padding-bottom: 30px;
+    font-family: 'Montserrat', serif;
+`;
+
+const Text = styled.div`
+    font-size: 35px;
+    color: rosybrown;
+    padding-top: 15px;
+    padding-bottom: 15px;
+    font-family: 'Roboto Slab', sans-serif;
+`
+
+const Introduce = styled.div`
+    font-size: 50px;
+    color: dimgray;
+`;
+
+const SchoolLogo = styled.img`
+    width: 1000px;
+    height: 280px;
+`;
+
+const Button = styled.button`
+    color: lightslategrey;
+    width: 200px;
+    height: 80px;
+    font-size: 30px;
+    border-radius: 20px;
+    border: 10px solid;
+`;
+
+function PageOne(){
+    return( 
+        <Wrapper>
+            <Title><b>Rachel J<br></br>Grade 8<br></br>CI</b></Title>
+            <Introduce><b>Nice to meet you!</b></Introduce>
+            <Text><b>This is a website.</b></Text>
+            <SchoolLogo src = "https://i.vimeocdn.com/portfolio_header/437682"></SchoolLogo>
+        </Wrapper>
+    )
+};
+
+export default PageOne
